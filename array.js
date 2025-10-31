6882c39c4ca5d2842817fa6431f190c869103806
imtiyaazshaboodien07-dev <imtiyaazshaboodien07@gmail.com>
2025-10-31 14:06:34 +0200

    exercise3

---
+++ array.js
@@ -0,0 +1,12 @@
+let colors =[];
+
+colors.push("green", "red", "blue",);
+
+let numbers = [1, 2, 3, 4, 5,];
+
+colors.pop();
+
+colors.unshift("purple");
+
+console.log("lengths of colors", colors.length);
+console.log("length of numbers", numbers.length);
